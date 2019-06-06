15061185e1c1e369929b383388642d140dc47845
LEpessoa <lempb.pessoa@gmail.com>
2019-06-06 16:07:21 -0300

    v0 do trabalho 3

---
+++ Trabalho 3/Nodo.py
@@ -0,0 +1,61 @@
+import socket
+import time
+import threading
+
+#Cria o socket, AF_INET é IPv4.
+#sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#gethostname() seta o ip do socket como sendo o ip da máquina local (127.0.0.1). O "0" faz com que o OS escolha uma porta livre por conta própria.
+s.bind(("127.0.0.1", 0)) 
+#Retorna uma tupla IP, porta.
+port = s.getsockname()[1]
+print("esta é a porta " + str(port))
+f = open("port_list.txt", "a")
+f.write(str(port) + '\n')
+f.close()
+DEVO_MORRER = False
+f = open("port_list.txt", "r")
+print(f.read())
+f.close()
+
+#Setup inicial do Nodo terminado, temos uma porta e ela está anotada no arquivo de referência.
+def escutando_mensagem():
+    print("iniciando...\n")
+    while True:
+        global DEVO_MORRER
+        if DEVO_MORRER == True :
+            break
+        #buffer size is 1024 bytes. "data" is the message received, "addr" is the port of the sending process.
+        print("escutando...\n")
+        data, addr = s.recvfrom(1024)
+        print ("Mensagem recebida: " + str(data) + "de "+ str(addr) + "\n")     
+
+def enviando_mensagem():    
+    while True:
+        global DEVO_MORRER
+        if DEVO_MORRER == True :
+            break
+        time.sleep(0.2)
+        message = input("O que devo fazer? ")
+        #Closes process if the message received was close.
+        if message == "close":
+            DEVO_MORRER = True
+        #opens the ports file with the "r" option, read_only.
+        ports = open("port_list.txt", "r")
+        #iterates over every line in the file. For python each line works as an element in an array.
+        for node_port in ports:            
+            s.sendto(message.encode("utf-8"), ("127.0.0.1", int(node_port))) 
+            print("Mensagem enviada para " + node_port)        
+         
+# CHAMANDO THREADS
+if __name__ == "__main__":
+    #Cria as threads
+    escutando_thread = threading.Thread(target=escutando_mensagem, args=())
+    enviando_thread = threading.Thread(target=enviando_mensagem, args=())
+    #Inicia as threads
+    enviando_thread.start()
+    escutando_thread.start()
+    
+    #Faz com que main espere as threads finalzarem antes de fechar.
+    #escutando_thread.join()
+    #enviando_thread.join()
