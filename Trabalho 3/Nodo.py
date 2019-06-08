27c12125289f979ba5e3463049944479885e7d0f
vinicius-alves <vinicius.alves@poli.ufrj.br>
2019-06-08 16:57:00 -0300

    Checagem do líder funcionando - reestruturação das threads

--- Trabalho 3/Nodo.py
+++ Trabalho 3/Nodo.py
@@ -1,18 +1,29 @@
 import socket
 import time
 import threading
+from asyncio import Semaphore
 
-#É setada em True para avisar todas as threads que devem iniciar harakiri.
+# É setada em True para avisar todas as threads que devem iniciar harakiri.
 DEVO_MORRER = False
 
-#Define se o processo é ou não o rei.
-KING = 0
+# Identificação do rei
+KING_ID = -1
+KING_CHECKED = False
+WAITING_KING = False
+KING_SEMAPHORE = Semaphore(0)
 
-#Cria o socket, AF_INET é IPv4.
-#sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+MSG_VIVO = 'VIVO'
+MSG_VIVO_OK = 'VIVO_OK'
+
+MSG_CLOSE = 'CLOSE'
+
+
+# Cria o socket, AF_INET é IPv4.
+# sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-#O "0"  no luga da porta faz com que o OS escolha uma porta livre por conta própria.
+# O "0"  no luga da porta faz com que o OS escolha uma porta livre por conta própria.
 s.bind(("127.0.0.1", 0))
 
 #Retorna uma tupla IP, porta.
@@ -22,31 +33,37 @@ f = open("port_list.txt", "a")
 f.write(str(MY_PORT) + "\n")
 f.close()
 
-#Função que procura pelo rei, enviando mensagens para todas as portas da lista.
-def find_the_king():
-    global KING
-    print("Tentando achar o rei\n")
-    f = open("port_list.txt", "r")
-    message = "king?"
-    for node_port in f:
-        s.sendto(message.encode("utf-8"), ("127.0.0.1", int(node_port)))
-        for i in range(10):
-            print("Tentativa " + str(i) + "...\n")
-            try:
-                message, address = s.recvfrom(1024)
-            except socket.timeout:
-                #Continua para próxima tentativa se falhar.                
-                continue 
-        if data == "sim" and addr == node_port:
-            KING = node_port
+# Função que verifica o rei
+def thread_que_verifica_king():
+
+    global KING_ID
+    global KING_CHECKED
+    global MSG_VIVO
+    global WAITING_KING
+    
+    while not(DEVO_MORRER):
+
+        KING_CHECKED = False
+        WAITING_KING = True
+        print("Tentando verificar o rei\n")
+        enviar_mensagem(MSG_VIVO, node_port = KING_ID)
+
+        time.sleep(8)
+
+        if(not(KING_CHECKED)):
+            print("The king is dead!!\nAnarchy! ")
             break
+            #KING_SEMAPHORE.acquire()
+
+        
 
 #Após escrever no arquivo, abre e lê para verificar se a primeira linha escrita é sua própria porta.
 f = open("port_list.txt", "r")
-lines = f.read()
-if str(lines[0].strip("\n")) == str(MY_PORT):
-    KING = MY_PORT
-    print("I'm king of port", KING)
+ports_list = list(f)
+# Primeiro rei é o primeiro a escrever
+KING_ID = int(ports_list[0])
+print("The king is", KING_ID)
+
 print(f.read())
 f.close()
 
@@ -55,38 +72,76 @@ f.close()
 #################################################################
 
 # Definição das funções que serão rodadas nas threads.
-def escutando_mensagem():
+def thread_escuta_mensangens():
     print("escutando...\n")
-    while True:
-        global DEVO_MORRER
-        if DEVO_MORRER == True :
-            sair_da_lista()
-            break
 
-        #buffer size is 1024 bytes. "data" is the message received, "addr" is the port of the sending process.        
-        data, addr = s.recvfrom(1024)
-        print ("Mensagem recebida: " + str(data) + "de "+ str(addr) + "\n")
+    global DEVO_MORRER
+    global WAITING_KING
+    global KING_CHECKED
+    global KING_ID
 
-def enviando_mensagem():    
-    while True:
-        global DEVO_MORRER
-        if DEVO_MORRER == True :
-            break
-        time.sleep(0.2)
+    while not(DEVO_MORRER):
+        
+        #buffer size is 1024 bytes. "data" is the message received, "addr" is the address of the sending process.       
+        try: 
+            data, addr = s.recvfrom(1024)
+            addr_port = addr[1]
+            message = data.decode("utf-8") 
+
+            print ("Mensagem recebida: " + message + " de "+ str(addr) + "\n")
+
+            if(message == MSG_VIVO ):
+                enviar_mensagem(MSG_VIVO_OK, node_port = addr_port)
+
+            elif(message == MSG_VIVO_OK and addr_port == KING_ID and WAITING_KING):
+                KING_CHECKED = True
+                WAITING_KING = False
+
+            elif(message == MSG_CLOSE):
+                DEVO_MORRER = True
+                sair_da_lista()
+        except:
+            pass
+
+
+def thread_interface():
+
+    global DEVO_MORRER
+    global MSG_CLOSE
+
+    while not(DEVO_MORRER):
+
         message = input("O que devo fazer? ")
+        message = message.replace(' ','')
 
-        #Closes process if the message received was close.
-        if message == "close":
-            DEVO_MORRER = True
+        enviar_mensagem(message, broadcast = True)
+        time.sleep(0.2)
 
-        #opens the ports file with the "r" option, read_only.
-        f = open("port_list.txt", "r")
-        ports = list(f)
-        #iterates over every line in the file. For python each line works as an element in an array.
-        for node_port in ports:
+
+
+def enviar_mensagem(message, node_port=-1, broadcast = False): 
+
+    try:
+
+        if(broadcast):
+            #opens the ports file with the "r" option, read_only.
+            f = open("port_list.txt", "r")
+            ports = list(f)
+            #iterates over every line in the file. For python each line works as an element in an array.
+            for node in ports:
+                s.sendto(message.encode("utf-8"), ("127.0.0.1", int(node)))
+                print ("Mensagem enviada: " + str(message) + " para todos os nós"+ "\n")
+            f.close()
+
+        else:
             s.sendto(message.encode("utf-8"), ("127.0.0.1", int(node_port)))
-            print("Mensagem enviada para " + str(node_port))
-        f.close()
+            print ("Mensagem enviada: " + str(message) + " para "+ str(node_port) + "\n")
+    except:
+        if(broadcast):
+            print("Ocorreu um erro ao enviar " + message+" para o nó "+str(node_port))
+        else:
+            print("Ocorreu um erro ao enviar " + message+" em broadcast")
+
 
 def sair_da_lista():
     print("Programa fechando")
@@ -110,13 +165,23 @@ def sair_da_lista():
 if __name__ == "__main__":
 
     #Cria as threads
-    escutando_thread = threading.Thread(target=escutando_mensagem, args=())
-    enviando_thread = threading.Thread(target=enviando_mensagem, args=())
+
+    # Thread responsável por receber mensagens e tomar ações.
+    thread_escuta_mensangens = threading.Thread(target=thread_escuta_mensangens, args=())
+
+    # Thread responsável por gerenciar interface com usuário
+    thread_interface = threading.Thread(target=thread_interface, args=())
+
+    # Thread responsável em detectar a presença do líder
+    thread_que_verifica_king = threading.Thread(target=thread_que_verifica_king, args=())
+
 
     #Inicia as threads
-    enviando_thread.start()
-    escutando_thread.start()
+    thread_interface.start()
+    thread_escuta_mensangens.start()
+    thread_que_verifica_king.start()
     
     #Faz com que main espere as threads finalizarem antes de fechar.
-    #escutando_thread.join()
-    #enviando_thread.join()    
+    thread_interface.join()
+
+    message = input("Saindo.. Pressione enter para continuar... ")
